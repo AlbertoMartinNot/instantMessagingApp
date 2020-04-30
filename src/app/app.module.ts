6a9a60c55b808d44586370e49ac9227a865e6add
Alberto Martin Notario <amnotario@tcpsi.es>
2020-04-30 17:56:13 +0200

    UserList changes, icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { FormsModule } from '@angular/forms';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
